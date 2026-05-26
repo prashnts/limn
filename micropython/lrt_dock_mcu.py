9c52bc8608db0098dfd0f8d69a4cc4f4702f7c64
Prashant Sinha <prashant@noop.pw>
2026-05-26 17:05:41 +0200

    add upy firmware for the dock

---
+++ micropython/lrt_dock_mcu.py
@@ -0,0 +1,108 @@
+# Limn Resistive Touch Probe
+# 
+# Copyright (C) 2026 Prashant Sinha <limn@noop.pw>
+# This file may be distributed under the terms of the GNU GPLv3 license.
+import time
+import select
+import sys
+from machine import UART, Pin, ADC, Timer
+from neopixel import NeoPixel
+
+# uart_in = UART(1, 115200, timeout=10)
+uart_in = sys.stdin.buffer
+uart_out = UART(0, 115200, timeout=0, tx=Pin(12), rx=Pin(13))
+npx = NeoPixel(Pin(16), 1)
+PIN_PROBE_OUT = Pin(11, Pin.OUT, Pin.PULL_DOWN)
+PIN_PWR_ON = Pin(8, Pin.OUT, Pin.PULL_DOWN)
+PIN_PWR_OFF = Pin(7, Pin.OUT, Pin.PULL_DOWN)
+ADC_DETECT = ADC(Pin(29, Pin.IN))
+
+POWER_STATE = False
+
+_enable_debug = True
+TAG = ">>>LRT>>>"
+EMBLEM = "Limn Resistive Touch Probe v1"
+
+def teeprint(info, line):
+    line = TAG + info + '>>>' + line + ">>>"
+    if _enable_debug:
+        print(line)
+    # uart_in.write((line + '\n').encode())
+
+def _pulse_power_pin(pin):
+    pin.on()
+    time.sleep_ms(100)
+    pin.off()
+
+def turn_on_power():
+    _pulse_power_pin(PIN_PWR_ON)
+    teeprint("power", "turned on")
+
+def turn_off_power():
+    _pulse_power_pin(PIN_PWR_OFF)
+    teeprint("power", "turned off")
+
+def ping(t):
+    teeprint("ping", f"t={time.ticks_ms()}")
+    npx[0] = (0, 80, 20)
+    npx.write()
+    time.sleep_ms(100)
+    npx[0] = (0, 0, 0)
+    npx.write()
+
+
+teeprint("booting", EMBLEM)
+
+timer_hello = Timer(-1)
+timer_hello.init(period=5000, mode=Timer.PERIODIC, callback=ping)
+
+
+turn_off_power()
+
+while True:
+    _c = select.select([uart_in], [], [], 0.01)
+    if _c[0]:
+        chars = ''
+        while True:
+            chr = uart_in.read(1)
+            if chr == b'\r' or chr == b'\n':
+                break
+            chars += chr.decode()
+        cmd = chars.strip()
+        print(f"Received command: {cmd}")
+
+        npx[0] = (80, 40, 10)
+        npx.write()
+        if 'power_on()' in cmd:
+            turn_on_power()
+        if 'power_off()' in cmd:
+            turn_off_power()
+
+    try:
+        chain_data = uart_out.readline()
+        if chain_data:
+            npx[0] = (120, 0, 50)
+            npx.write()
+            print(chain_data.decode())
+    except Exception:
+        teeprint("error", "failed to read from uart_out")
+
+    detect_value = ADC_DETECT.read_u16()
+
+    if detect_value > 32000 and detect_value < 33000 and not POWER_STATE:
+        teeprint("probe_detected", f"{detect_value=}")
+        time.sleep(1)
+        detect_value = ADC_DETECT.read_u16()
+        if detect_value > 32000 and detect_value < 33000:
+            turn_on_power()
+        POWER_STATE = True
+    if detect_value > 55000 and POWER_STATE:
+        teeprint("probe_removed", f"{detect_value=}")
+        turn_off_power()
+        POWER_STATE = False
+
+    # time.sleep_ms(50)
+    npx[0] = (0, 0, 0) if not POWER_STATE else (0, 80, 20)
+    npx.write()
+
+    
